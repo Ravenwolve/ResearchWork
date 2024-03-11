1426e2801a55d5374a6dff941d5b0fa70058b42e
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2024-03-11 19:14:28 +0400

    Update Graph.cpp

--- GraphTheory/Graph.cpp
+++ GraphTheory/Graph.cpp
@@ -24,14 +24,14 @@ inline size_t Graph<Directing, Weighting>::Size() const noexcept {
 }
 
 template <bool Directing, bool Weighting>
-inline Graph<Directing, Weighting>::Graph(const Graph<false, Weighting>& other) noexcept : adjacencyList(other.adjacencyList), visited(other.visited), cachedFloyd(other.cachedFloyd) {}
+inline Graph<Directing, Weighting>::Graph(const Graph<false, Weighting>& other) noexcept : adjacencyList(other.adjacencyList), visited(other.visited) {}
 
 template <bool Directing, bool Weighting>
 inline Graph<Directing, Weighting>::Graph() noexcept {}
 
 
 template <bool Directing, bool Weighting>
-inline Graph<Directing, Weighting>::Graph(const Graph<true, Weighting>& other) noexcept : adjacencyList(other.adjacencyList), visited(other.visited), cachedFloyd(other.cachedFloyd) {
+inline Graph<Directing, Weighting>::Graph(const Graph<true, Weighting>& other) noexcept : adjacencyList(other.adjacencyList), visited(other.visited) {
 	if constexpr (!Directing) {
 		for (auto iterList = adjacencyList.begin(); iterList != adjacencyList.end(); ++iterList)
 			for (auto iterVertex = iterList->second.begin(); iterVertex != iterList->second.end(); ++iterVertex) {
@@ -51,7 +51,6 @@ inline void Graph<Directing, Weighting>::AddVertex(const std::string& vertex) {
 		throw "Vertex " + vertex + " already exists";
 	adjacencyList[vertex] = WeightingType<Weighting>();
 	visited[vertex] = false;
-	cachedFloyd.clear();
 }
 
 template <bool Directing, bool Weighting>
@@ -72,7 +71,6 @@ inline void Graph<Directing, Weighting>::AddArc(const std::string& firstVertex, 
 	AddA(firstVertex, secondVertex, weightForWeightedGraph);
 	if constexpr (!Directing)
 		AddA(secondVertex, firstVertex, weightForWeightedGraph);
-	cachedFloyd.clear();
 }
 
 template <bool Directing, bool Weighting>
@@ -86,7 +84,6 @@ inline void Graph<Directing, Weighting>::RemoveArc(const std::string& firstVerte
 	adjacencyList[firstVertex].erase(secondVertex);
 	if constexpr (!Directing)
 		adjacencyList[secondVertex].erase(firstVertex);
-	cachedFloyd.clear();
 }
 
 template <bool Directing, bool Weighting>
@@ -98,55 +95,6 @@ inline void Graph<Directing, Weighting>::RemoveVertex(const std::string& vertex)
 			RemoveArc(iter->first, vertex);
 	adjacencyList.erase(vertex);
 	visited.erase(vertex);
-	cachedFloyd.clear();
-}
-
-template <bool Directing, bool Weighting>
-inline void Graph<Directing, Weighting>::DFS(const std::string& vertex) const noexcept {
-	std::stack<std::string> stack;
-	std::string curVertex;
-	visited[vertex] = true;
-	stack.push(vertex);
-	while (!stack.empty()) {
-		curVertex = stack.top();
-		stack.pop();
-		for (auto item : adjacencyList.at(curVertex)) {
-			if constexpr (Weighting) {
-				if (!visited[item.first]) {
-					stack.push(item.first);
-					visited[item.first] = true;
-				}
-			}
-			else if (!visited[item]) {
-				stack.push(item);
-				visited[item] = true;
-			}
-		}
-	}
-}
-
-template <bool Directing, bool Weighting>
-inline void Graph<Directing, Weighting>::BFS(const std::string& vertex) const noexcept {
-	std::queue<std::string> queue;
-	std::string curVertex;
-	queue.push(vertex);
-	visited[vertex] = true;
-	while (!queue.empty()) {
-		curVertex = queue.front();
-		queue.pop();
-		for (auto item : adjacencyList.at(curVertex)) {
-			if constexpr (Weighting) {
-				if (!visited[item.first]) {
-					queue.push(item.first);
-					visited[item.first] = true;
-				}
-			}
-			else if (!visited[item]) {
-				queue.push(item);
-				visited[item] = true;
-			}
-		}
-	}
 }
 
 template <bool Directing, bool Weighting>
@@ -156,57 +104,28 @@ inline void Graph<Directing, Weighting>::ResetVisited() const noexcept {
 }
 
 template <bool Directing, bool Weighting>
-inline int Graph<Directing, Weighting>::IsLinked() const noexcept {
-	if constexpr (!Directing) {
-		int result = 1;
-		DFS(adjacencyList.begin()->first);
-		for (auto iter = visited.begin(); iter != visited.end(); ++iter) {
-			if (!iter->second)
-				result = 0;
-			else iter->second = false;
-		}
-		return result;
-	}
-	else {
-		bool strongConnect;
-		for (auto list : adjacencyList) {
-			strongConnect = true;
-			DFS(list.first);
-			for (auto iter = visited.begin(); iter != visited.end(); ++iter) {
-				if (!iter->second)
-					strongConnect = false;
-				else iter->second = false;
-			}
-			if (strongConnect) // если это сильно-связный орграф
-				return 1;
-		}
-		return -Graph<0, Weighting>(*this).IsLinked(); // проверка на слабую связность (0 или -1)
-	}
-}
-
-template <bool Directing, bool Weighting>
 inline void GraphIO::Input(Graph<Directing, Weighting>& graph, std::ifstream& input) {
 	bool d, w;
 	input >> d >> w;
 	if (d != Directing || w != Weighting)
 		throw "Input graph doesn't match the declared type";
 	std::string firstStr, secondStr;
-	input.get();
-	while (input.get() != '\n') {
-		input >> firstStr;
-		graph.AddVertex(firstStr);
-	}
 	double weight = 1;
-	for (input >> firstStr; !input.eof(); input >> firstStr)
+	for (input >> firstStr; !input.eof(); input >> firstStr) {
+		if (!graph.adjacencyList.contains(firstStr))
+			graph.AddVertex(firstStr);
 		while (input.get() != '\n') {
 			input >> secondStr;
 			if constexpr (Weighting)
 				input >> weight;
 			try {
+				if (!graph.adjacencyList.contains(secondStr))
+					graph.AddVertex(secondStr);
 				graph.AddArc(firstStr, secondStr, weight);
 			}
 			catch (...) {}
 		}
+	}
 }
 
 template <bool Directing, bool Weighting>
@@ -248,7 +167,7 @@ inline std::unordered_map<std::string, std::string> Graph<Directing, Weighting>:
 						if (lengthsOfShortestWaysToAll.contains(iter->first)) {
 							if (lengthsOfShortestWaysToAll[*currentVertex] + adjacencyList.at(*currentVertex).at(iter->first) < lengthsOfShortestWaysToAll[iter->first]) {
 								lengthsOfShortestWaysToAll[iter->first] = lengthsOfShortestWaysToAll[*currentVertex] + adjacencyList.at(*currentVertex).at(iter->first);
-								shortestWaysToAll[iter->first] = *currentVertex; // добавляем ребро (w, v)
+								shortestWaysToAll[iter->first] = *currentVertex; // РґРѕР±Р°РІР»СЏРµРј СЂРµР±СЂРѕ (w, v)
 							}
 						}
 						else {
@@ -256,7 +175,7 @@ inline std::unordered_map<std::string, std::string> Graph<Directing, Weighting>:
 							shortestWaysToAll[iter->first] = *currentVertex;
 						}
 					}
-					// ищем минимальный элемент из D из оставшихся
+					// РёС‰РµРј РјРёРЅРёРјР°Р»СЊРЅС‹Р№ СЌР»РµРјРµРЅС‚ РёР· D РёР· РѕСЃС‚Р°РІС€РёС…СЃСЏ
 					if (lengthsOfShortestWaysToAll.contains(iter->first) && lengthsOfShortestWaysToAll[iter->first] < minFromSWTWT) {
 						nextVertex = const_cast<std::string*>(&iter->first);
 						minFromSWTWT = lengthsOfShortestWaysToAll[iter->first];
@@ -304,128 +223,38 @@ inline std::unordered_map<std::string, std::unordered_map<std::string, double>> 
 
 template <bool Directing, bool Weighting>
 inline std::unordered_map<std::string, std::unordered_map<std::string, std::string>>& Graph<Directing, Weighting>::Floyd() const {
-	if (cachedFloyd.empty()) {
-		std::unordered_map<std::string, std::unordered_map<std::string, double>> adjacencyMatrix = GetAdjacencyMatrix();
-		cachedFloyd = std::unordered_map<std::string, std::unordered_map<std::string, std::string>>();
-		for (auto k_iter = adjacencyMatrix.begin(); k_iter != adjacencyMatrix.end(); ++k_iter)
-			for (auto i_iter = adjacencyMatrix.begin(); i_iter != adjacencyMatrix.end(); ++i_iter)
-				for (auto j_iter = i_iter->second.begin(); j_iter != i_iter->second.end(); ++j_iter)
-					// A[i, j] = min(A[i, j], A[i, k] + A[k, j])
-					if (i_iter->second[k_iter->first] + k_iter->second[j_iter->first] < j_iter->second) {
-						j_iter->second = i_iter->second[k_iter->first] + k_iter->second[j_iter->first];
-						cachedFloyd[i_iter->first][j_iter->first] = k_iter->first;
-					}
-	}
-	return cachedFloyd;
+	std::unordered_map<std::string, std::unordered_map<std::string, double>> adjacencyMatrix = GetAdjacencyMatrix();
+	std::unordered_map<std::string, std::unordered_map<std::string, std::string>> floydMatrix;
+	for (auto k_iter = adjacencyMatrix.begin(); k_iter != adjacencyMatrix.end(); ++k_iter)
+		for (auto i_iter = adjacencyMatrix.begin(); i_iter != adjacencyMatrix.end(); ++i_iter)
+			for (auto j_iter = i_iter->second.begin(); j_iter != i_iter->second.end(); ++j_iter)
+				// A[i, j] = min(A[i, j], A[i, k] + A[k, j])
+				if (i_iter->second[k_iter->first] + k_iter->second[j_iter->first] < j_iter->second) {
+					j_iter->second = i_iter->second[k_iter->first] + k_iter->second[j_iter->first];
+					floydMatrix[i_iter->first][j_iter->first] = k_iter->first;
+				}
+	return floydMatrix;
 }
 
 template<bool Directing, bool Weighting>
-inline std::unordered_map<std::string, std::unordered_map<std::string, std::string>> Graph<Directing, Weighting>::Johnson()
-{
+inline std::unordered_map<std::string, std::unordered_map<std::string, std::string>> Graph<Directing, Weighting>::Johnson() {
 	std::unordered_map<std::string, std::unordered_map<std::string, std::string>> dists;
 	AddVertex("s");
 	for (auto it = adjacencyList.begin(); it != adjacencyList.end(); ++it)
-	{
 		AddArc("s", it->first, 0.f);
-	}
-
 	auto tempBellmanDists = FordBellman("s");
-
-	if constexpr (Weighting)
-	{
+	if constexpr (Weighting) {
 		for (auto u_iter = adjacencyList.begin(); u_iter != adjacencyList.end(); ++u_iter)
-		{
 			for (auto v_iter = u_iter->second.begin(); v_iter != u_iter->second.end(); ++v_iter)
-			{
 				v_iter->second = v_iter->second +
 					tempBellmanDists[u_iter->first] - tempBellmanDists[v_iter->first];
-			}
-		}
 	}
-	else
-	{
-		throw std::exception("Graph must be oriented");
-	}
-	
-
+	else throw std::exception("Graph must be oriented");
 	RemoveVertex("s");
-
 	for (auto it = adjacencyList.begin(); it != adjacencyList.end(); ++it)
-	{
 		dists[it->first] = Dijkstra(it->first);
-	}
-
 	return dists;
 }
-
-template <bool Directing, bool Weighting>
-inline std::vector<std::string> Graph<Directing, Weighting>::ShortestPath(const std::string& fromWhere, const std::string& whereTo, ShortestPathAlgorithm algorithmType) const {
-	if (!adjacencyList.contains(fromWhere))
-		throw "Vertex " + fromWhere + " doesn't exist";
-	else if (!adjacencyList.contains(whereTo))
-		throw "Vertex " + whereTo + " doesn't exist";
-	if constexpr (Weighting) {
-		if (algorithmType == ShortestPathAlgorithm::Default && cachedFloyd.empty() || algorithmType == ShortestPathAlgorithm::Dijkstra) {
-			std::unordered_map<std::string, std::string> result = Dijkstra(fromWhere);
-			if (!result.contains(whereTo))
-				throw "Path from " + fromWhere + " to " + whereTo + " doesn't exist";
-			std::vector<std::string> path;
-			std::stack<std::string> stack;
-			// поскольку нас интересует заданная вершина, до которой есть путь, то мы от этой вершины и отталкиваемся,
-			// перечисляя обратные дуги, то есть вершины в обратном порядке
-			std::string* cur = const_cast<std::string*>(&whereTo);
-			while (true) {
-				stack.push(*cur);
-				if (*cur == fromWhere)
-					break;
-				cur = &result[*cur];
-			}
-			while (!stack.empty()) {
-				path.push_back(stack.top());
-				stack.pop();
-			}
-			return path;
-		}
-		else if (algorithmType == ShortestPathAlgorithm::Default || algorithmType == ShortestPathAlgorithm::Floyd) {
-			std::unordered_map<std::string, std::unordered_map<std::string, std::string>> result = Floyd();
-			if (!result[fromWhere].contains(whereTo)) {
-				if (!adjacencyList.at(fromWhere).contains(whereTo))
-					throw "Path from " + fromWhere + " to " + whereTo + " doesn't exist";
-				else {
-					// Алгоритм не учитывает рёбра, соединяющие вершины напрямую, поэтому это проверка на наличие такого ребра
-					std::vector<std::string> path(2);
-					path[0] = fromWhere;
-					path[1] = whereTo;
-					return path;
-				}
-			}
-			std::vector<std::string> path;
-			std::function<void(const std::string&, const std::string&)> floydPath = [&floydPath, &result, &path, &fromWhere, &whereTo](const std::string& fw, const std::string& wt) {
-				if (!result[fw].contains(wt))
-					return;
-				floydPath(fw, result[fw][wt]);
-				path.push_back(result[fw][wt]);
-				floydPath(result[fw][wt], wt);
-			};
-			path.push_back(fromWhere);
-			floydPath(fromWhere, whereTo);
-			path.push_back(whereTo);
-			return path;
-		}
-		else {
-			//std::unordered_map<std::string, std::string> result = FordBellman(fromWhere);
-			//if (result[whereTo] == "")
-				//throw "Path from " + fromWhere + " to " + whereTo + " doesn't exist";
-			std::vector<std::string> path;
-			//for (std::string* currentVertex = const_cast<std::string*>(&whereTo); *currentVertex != ""; currentVertex = &result[*currentVertex])
-				//path.push_back(*currentVertex);
-			//std::reverse(path.begin(), path.end());
-			return path;
-		}
-	}
-	else throw "This graph is unweighted";
-}
-
 
 template <bool Directing, bool Weighting>
 inline std::unordered_map<std::string, double> Graph<Directing, Weighting>::FordBellman(const std::string& fromWhere) const {
@@ -453,18 +282,6 @@ inline std::unordered_map<std::string, double> Graph<Directing, Weighting>::Ford
 		else throw std::string("This graph having negative cycle");
 	}
 	else throw std::string("This graph is unweighted");
-}
-
-template <bool Directing, bool Weighting>
-inline double Graph<Directing, Weighting>::Distance(const std::string& fromWhere, const std::string& whereTo, ShortestPathAlgorithm algorithmType) const {
-	std::vector<std::string> path = ShortestPath(fromWhere, whereTo, algorithmType);
-	if constexpr (Weighting) {
-		double length = 0;
-		for (int i = 1; i < path.size(); ++i)
-			length += adjacencyList.at(path[i - 1]).at(path[i]);
-		return length;
-	}
-	else return path.size();
 }
 
 template <bool Directing, bool Weighting>
